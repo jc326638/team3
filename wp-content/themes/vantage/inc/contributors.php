71782658c75ddf60bd5d494e22dfd7b7b4e2218e
jc313512 <manjinderkaur.khosa@my.jcu.edu.au>
2016-10-17 20:43:40 +1000

    complete website
    Our group website is completed by all team members.

---
+++ wp-content/themes/vantage/inc/contributors.php
@@ -0,0 +1,45 @@
+<?php
+return json_decode( '{
+	"77e88891e4965161953320ec66623cbc": {
+		"name": "Greg Priday",
+		"email": "77e88891e4965161953320ec66623cbc",
+		"loc": 27697,
+		"score": 1888.9883572624626,
+		"percent": 77.86846354815614
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 3114,
+		"score": 324.1160939383235,
+		"percent": 13.360814082932107
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 3683,
+		"score": 142.44319613578398,
+		"percent": 5.871837580860756
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 657,
+		"score": 69.39095784748515,
+		"percent": 2.8604555718644775
+	},
+	"07294cc1dd7658895e44c559dfffd76f": {
+		"name": "gpriday",
+		"email": "07294cc1dd7658895e44c559dfffd76f",
+		"loc": 29,
+		"score": 0.7270623526278086,
+		"percent": 0.029971189650359952
+	},
+	"43c0e7220c23bd6636cf5469be6e9612": {
+		"name": "Braam Genis",
+		"email": "43c0e7220c23bd6636cf5469be6e9612",
+		"loc": 7,
+		"score": 0.20518080008493006,
+		"percent": 0.008458026536145016
+	}
+}', true );
